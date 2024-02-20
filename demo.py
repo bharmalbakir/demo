a5938c0aa1d792c9b8744eabf46281b0ed451179
Mahamadulbakir <clfmahamadulbakir24@gmail.com>
2024-02-20 16:31:19 +0530

    First Commit

---
+++ demo.py
@@ -0,0 +1,94 @@
+# import re
+# txt=" m n a sjdc c wnfjcnw c ew "
+# x=re.sub("\s","2",txt)
+# print(x)
+
+
+# import datetime
+# x=datetime.datetime.now()
+# print (x.year)
+
+
+
+
+# colors={"black","blue","green",}
+# colors .add ("red")
+# colors .add ("blue")
+# print(colors)
+
+
+
+# class Glasses:
+#     def printShadeIndex(self):
+#         print("low")
+# class Sunglasses(Glasses):
+#     def printShadeIndex(self):
+#         print("high")
+
+# glasses= Glasses()
+# glasses.printShadeIndex()
+
+
+# x=0
+# y=7
+# while x+y < 10:
+#     x += 1
+#     print(x, end='')
+
+
+# x=8
+# y=2
+# print("\r")
+# print(x/y)
+# print(x//y)
+
+
+# a=5 and 10
+# b=5 or 10
+# c = a+b
+# print(c*2)
+
+
+# class Circle:
+#     pass
+# Circle.pi = 3.14
+# print(Circle.pi)
+# Circle.pi = 3.1415
+
+
+
+
+
+# x="10"
+# for i in range(0,10):
+#     i=i+1
+#     print(x*i)
+
+
+
+
+
+
+# a="10"
+# for i in range(0,10):
+#     i=i*a
+#     print(i)
+
+
+# class distance:
+#   def __init__(self, x=None,y=None):
+#       self.ft=x
+#       self.inch=y
+#   def __ge__(self, x):
+#       val1=self.ft*12+self.inch
+#       val2=x.ft*12+x.inch
+#       if val1>=val2:
+#           return True
+#       else:
+#           return False
+          
+          
+# d1=distance(2,1)
+# d2=distance(4,10)
+# print(d1>=d2)
+
